539ad46a320c2846c686ab08736df105cdfefd46
Jan Tarczoń <jan.tarczon@student.uj.edu.pl>
2025-10-28 20:55:28 +0100

    Dodano funkcję zwracającą sumę po wierszach z trzech zbiorów liczb

--- zadanie1/app/src/HTTPServer.scala
+++ zadanie1/app/src/HTTPServer.scala
@@ -1,8 +1,16 @@
 package app
-object MinimalApplication extends cask.MainRoutes:
-  @cask.get("/")
-  def hello() =
-    "Hello World!"
+object HTTPServer extends cask.MainRoutes:
+  def add(summands: (Int,Int)): Int =
+    summands(0) + summands(1)
+
+  def sum(summands_1: Seq[Int], summands_2: Seq[Int]): Seq[Int] =
+    summands_1.zip(summands_2).map(add)
+     
+
+  @cask.postJson("/sum3")
+  def sum3(collection1: Seq[Int], collection2: Seq[Int], collection3: Seq[Int]) =
+    sum(collection1, sum(collection2, collection3))
+    
 
   def subtraction(values: (Int, Int)): Int =
     values(0) - values(1)
@@ -11,7 +19,7 @@ object MinimalApplication extends cask.MainRoutes:
   def doThing(values: Seq[Int], operator: String) =
     val predecessors = values.init
     val successors = values.tail
-    val difference = successors.zip(predecessors).map(subtraction)
+    val difference = successors.diff(predecessors)
 
     val returnValue = operator match
       case "<" => difference.forall(_ > 0)
