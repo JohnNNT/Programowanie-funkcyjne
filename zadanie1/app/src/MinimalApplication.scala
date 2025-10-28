35c1ae3b78599ea3ebc13e1106b6148856411320
Jan Tarczoń <jan.tarczon@student.uj.edu.pl>
2025-10-28 19:07:24 +0100

    Dodano funkcje isSorted sprawdzającą, czy zbiór liczb jest ułożony w sposób posortowany

--- zadanie1/app/src/MinimalApplication.scala
+++ zadanie1/app/src/MinimalApplication.scala
@@ -4,8 +4,19 @@ object MinimalApplication extends cask.MainRoutes:
   def hello() =
     "Hello World!"
 
-  @cask.post("/do-thing")
-  def doThing(request: cask.Request) =
-    request.text().reverse
+  def subtraction(values: (Int, Int)): Int =
+    values(0) - values(1)
+
+  @cask.postJson("/isSorted")
+  def doThing(values: Seq[Int], operator: ujson.value) =
+    val predecessors = values.init
+    val successors = values.tail
+    val difference = successors.zip(predecessors.zip).map(subtraction)
+
+    operator match
+      case "<" => difference.forall(_ > 0)
+      case "<=" => difference.forall(_ >= 0)
+      case ">" => difference.forall(_ < 0)
+      case _ => difference.forall(_ <= 0)
 
   initialize()
