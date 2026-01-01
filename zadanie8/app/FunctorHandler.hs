4f6920dab654290882570fb8b18762eba854ed1a
Jan Tarczoń <jan.tarczon@student.uj.edu.pl>
2026-01-02 00:24:12 +0100

    Dodano kod endpointa

--- zadanie8/app/FunctorHandler.hs
+++ zadanie8/app/FunctorHandler.hs
@@ -18,3 +18,28 @@ instance Show a => Show (MyMaybe a) where
     show None = "Nothing"
     show (Some a) = show a
     
+data FunctorEquationJSON = FunctorEquationJSON {
+    operation :: Text,
+    arg1 :: Int,
+    arg2 :: Int
+} deriving (Show, Generic)
+
+newtype EquationResultJSON = EquationResultJSON {
+    result :: Int
+} deriving (Show, Generic)
+
+instance ToJSON FunctorEquationJSON
+instance FromJSON FunctorEquationJSON
+
+instance ToJSON EquationResultJSON
+instance FromJSON EquationResultJSON
+
+performEquation :: Text -> Int -> Int -> MyMaybe Int
+performEquation "+" x y = fmap (+y) (Some x)
+performEquation "-" x y = fmap (+(-y)) (Some x)
+performEquation _ _ _ = Some 0
+
+performFunctorEquation :: ActionM ()
+performFunctorEquation = do
+    equationData <- jsonData :: ActionM FunctorEquationJSON
+    json $ (EquationResultJSON{result = read $ show $ performEquation (operation equationData) (arg1 equationData) (arg2 equationData)})    
