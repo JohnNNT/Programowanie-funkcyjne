1869be84aa3d0f66f1d49bc55fcee385874f3b80
Jan Tarczoń <jan.tarczon@student.uj.edu.pl>
2026-02-07 16:01:13 +0100

    Dodano funcję usuwania pocz atku listy

--- zadanie3/app/src/MinimalApplication.scala
+++ zadanie3/app/src/MinimalApplication.scala
@@ -1,13 +1,17 @@
 package app
+
+import cask.endpoints.JsonData.JsonDataImpl
+
 object MinimalApplication extends cask.MainRoutes:
   override def host: String = "0.0.0.0"
 
-  @cask.get("/")
-  def hello() =
-    "Hello World!"
+  @cask.postJson("/removeFirst")
+  def doThing(list: Seq[Int]) =
+    val noHead = list match
+      case Nil => Seq()
+      case head :: tail => tail
+      case _ => Seq()
 
-  @cask.post("/do-thing")
-  def doThing(request: cask.Request) =
-    request.text().reverse
+    upickle.write(noHead)
 
   initialize()
