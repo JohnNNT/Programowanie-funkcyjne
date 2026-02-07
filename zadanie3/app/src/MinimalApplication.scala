d5488ee8718829a9b3058c304e42ce545562e773
Jan Tarczoń <jan.tarczon@student.uj.edu.pl>
2026-02-07 18:25:52 +0100

    Dodano własną implementację listy

--- zadanie3/app/src/MinimalApplication.scala
+++ zadanie3/app/src/MinimalApplication.scala
@@ -4,12 +4,13 @@ import cask.endpoints.JsonData.JsonDataImpl
 import cask.model.Response.Data.NumericData
 import cask.model.Response.Data.WritableData
 
+import app.createList
 object MinimalApplication extends cask.MainRoutes:
   override def host: String = "0.0.0.0"
 
   @cask.postJson("/removeFirst")
   def doThing(list: Seq[Int]) =
-    val noHead = list match
+    val newList = list match
       case Nil => Seq()
       case head :: tail => tail
       case _ => Seq()
